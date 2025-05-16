538a92395e1f521fb00790f8385378c3b2b8abfd
Mike Yan Michelis <michelism@ethz.ch>
2025-05-16 16:28:47 +0200

    Feat: Create images in separate folder, add time axis to trajectory and sensor data

--- src/parquet_create.py
+++ src/parquet_create.py
@@ -1,29 +1,63 @@
+
+import os
+from PIL import Image
+from enum import Enum
+
 import pyarrow as pa
 import pyarrow.parquet as pq
 
 import pandas as pd
 import numpy as np
-import os
-
-from enum import Enum
-
 
 from utils import DatasetKeys
 
 
+
 N = 10
+
+### Create a directory with dummy images
+if not os.path.exists("imgs"):
+    os.makedirs("imgs")
+for i in range(N):
+    img = np.random.randint(0, 255, (64, 64, 3)).astype(np.uint8)
+    Image.fromarray(img).save(f"imgs/img_{i}.png")
+
+### Create robot trajectory and sensor data
+traj_data = []
+for i in range(N):
+    sampling_rate = 150
+    traj_length = np.random.randint(50, 200)
+    timestamp = np.arange(traj_length) / sampling_rate
+    traj = np.random.rand(traj_length, 3)
+
+    traj_data.append(np.concatenate((timestamp.reshape(-1,1), traj), axis=1).tolist())
+
+sensor_data = []
+for i in range(N):
+    sampling_rate = 50
+    traj_length = np.random.randint(10, 100)
+    timestamp = np.arange(traj_length) / sampling_rate
+    sensor = np.random.rand(traj_length, 7)
+
+    sensor_data.append(np.concatenate((timestamp.reshape(-1,1), sensor), axis=1).tolist())
+
+
+
 df = pd.DataFrame({
     DatasetKeys.TEXT.value: ["This is a test sentence."] * N,
-    # DatasetKeys.IMAGE.value: [np.random.rand(3, 64, 64).astype(np.float32)] * N,
-    DatasetKeys.ROBOT_TRAJ.value: [np.random.rand(np.random.randint(10, 100), 3).tolist() for _ in range(N)],
-    DatasetKeys.SENSOR_DATA.value: [np.random.rand(np.random.randint(10, 100), 7).tolist() for _ in range(N)],
+    # First entry of each list is the timestamp.
+    DatasetKeys.ROBOT_TRAJ.value: traj_data,
+    DatasetKeys.SENSOR_DATA.value: sensor_data,
+
+    # Loading images externally
+    DatasetKeys.IMAGE.value: [f"imgs/img_{i}.png" for i in range(N)],
 })
 
+### Store as Parquet file
 table = pa.Table.from_pandas(df)
 pq.write_table(table, "test.pq")
 
 new_table = pq.read_table("test.pq")
-
 
 breakpoint()
 
