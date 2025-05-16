70e877ea8e0d16540801a3cf6abc8075cd0b35ce
Mike Yan Michelis <michelism@ethz.ch>
2025-05-16 17:58:24 +0200

    Feat: Image included in from h5

--- src/parquet_fromh5.py
+++ src/parquet_fromh5.py
@@ -16,8 +16,9 @@ from utils import DatasetKeys
 txt_data = []
 act_data = []
 obs_data = []
+img_data = []
 
-filenames = ["20250325_170840.h5"]
+filenames = ["20250325_170840.h5", "20250325_171215.h5"]
 for filename in filenames:
     with h5py.File(filename, "r") as f:
         # Time in nanoseconds should be stored in seconds
@@ -25,20 +26,26 @@ for filename in filenames:
         for t in f['hand']['right']['policy_output']:
             a = f['hand']['right']['policy_output'][t]
             actions.append(np.concatenate([[int(t)/1e9], a]))
-        actions = np.array(actions)
+        actions = np.array(actions).astype(np.float32)
 
         observations = []
         for t in f['right']['franka']['end_effector_pose']:
             o = np.array(f['right']['franka']['end_effector_pose'][t])
             observations.append(np.concatenate([[int(t)/1e9], o]))
-        observations = np.array(observations)
+        observations = np.array(observations).astype(np.float32)
 
         # Temporary description
         text = list(f['task_description'])[0]
 
-        # for t in f['oakd_front_view']['color']:
-        #     img = np.array(f['oakd_front_view']['color'][t])
-        # Not storing individual frames for now.
+        # Temporary just store first image in sequence
+        for t in f['oakd_front_view']['color']:
+            img = np.array(f['oakd_front_view']['color'][t])
+            img = img.astype(np.uint8)
+            img = Image.fromarray(img)
+            img.save(f"data/imgs/{filename.split('.')[0]}.png")
+            break
+        img_data.append(f"data/imgs/{filename.split('.')[0]}.png")
+
 
     txt_data.append(text)
     act_data.append(actions.tolist())
@@ -53,7 +60,7 @@ df = pd.DataFrame({
     DatasetKeys.OBSERVATIONS.value: obs_data,
 
     # Loading images externally
-    # DatasetKeys.IMAGE.value: [f"data/imgs/img_{i}.png" for i in range(N)],
+    DatasetKeys.IMAGE.value: img_data,
 })
 
 table = pa.Table.from_pandas(df)
@@ -63,6 +70,8 @@ pq.write_table(table, data_path)
 loaded_table = pq.read_table(data_path)
 
 print(f"Table created and store as Parquet file: {data_path}")
+
+
 
 
 
