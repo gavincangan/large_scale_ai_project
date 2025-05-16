ef72412fec72efc82380245d79ec29d76c62ae31
Mike Yan Michelis <michelism@ethz.ch>
2025-05-16 16:02:37 +0200

    Feat: Nonempty utils file

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class DatasetKeys(Enum):
+    TEXT = "text"
+    IMAGE = "image"
+    AUDIO = "audio"
+    VIDEO = "video"
+    ROBOT_TRAJ = "robot_traj"
+    SENSOR_DATA = "sensor_data"
+
