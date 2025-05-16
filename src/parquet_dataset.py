68f7a5557c9a0c1ae3de72b4557b86c54d17d6aa
Barnabas Gavin Cangan <bgavin@vt.edu>
2025-05-16 15:46:27 +0200

    moved enum to utils

--- src/parquet_dataset.py
+++ src/parquet_dataset.py
@@ -5,30 +5,15 @@ import torch
 from enum import Enum
 
 
-class DatasetKeys(Enum):
-    TEXT = "text"
-    IMAGE = "image"
-    AUDIO = "audio"
-    VIDEO = "video"
-    ROBOT_TRAJ = "robot_traj"
-    SENSOR_DATA = "sensor_data"
-
-
-class ParquetDataset(Dataset):
-    def __init__(self, parquet_file: str, sequence_length: int):
+class TimeSeriesParquetDataset(Dataset):
+    def __init__(self, parquet_file: str, sequence_length: int, sample_rate: int = 1):
         self.parquet_ds = pq.read_table(parquet_file, memory_map=True)
         self.real_length = len(self.parquet_ds)
         self.sequence_length = sequence_length
+        self.sample_rate = sample_rate
 
     def __len__(self):
         return 0
 
     def __getitem__(self, idx: int):
-        sample_str = str(self.parquet_ds["text"][idx % self.real_length])
-        return self.tokenizer.encode_plus(
-            sample_str,
-            max_length=self.sequence_length + 1,
-            padding='max_length',
-            truncation=True,
-            padding_side="right"
-        )
+        pass
